a5d70df17862df0bb20a445a927a22347fc29c25
Lohos06 <122732120+Lohos06@users.noreply.github.com>
2026-01-27 11:48:47 +0100

    Email en fonction du form

--- actions/email.ts
+++ actions/email.ts
@@ -2,14 +2,15 @@
 
 import { Resend } from 'resend';
 
-export default async function email(){
+export default async function email(subjectValue:string, emailValue:string, messageValue:string){
 const resend = new Resend(process.env.RESEND_API_KEY);
-console.log(process.env.RESEND_API_KEY);
 const { data, error } = await resend.emails.send({
     from: 'onboarding@resend.dev',
     to: ['lorenzolh06@gmail.com'],
-    subject: 'Hello World',
-    html: '<strong>It works!</strong>',
+    subject: subjectValue,
+    html: 
+    `<strong>Email : ${emailValue}</strong>
+    <p>${messageValue}</p>`,
   });
 
   if (error) {
--- components/ContactForm/ContactForm.tsx
+++ components/ContactForm/ContactForm.tsx
@@ -2,13 +2,22 @@
 
 import email from "@/actions/email"
 import styles from "./ContactForm.module.css"
-import { FormEvent, use } from "react";
+import { FormEvent, useState } from "react";
 
 export default function ContactForm() {
+
+  const[subjectValue, setSubjectValue] = useState("client")
+  const[emailValue, setEmailValue] = useState("")
+  const[messageValue, setMessageValue] = useState("")
+
+  const [submitMessage, setSubmitMessage] = useState("");
+
   const submit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault(); // Prevent default form submission
-    email();
-  }   
+    e.preventDefault();
+    email(subjectValue, emailValue, messageValue);
+    console.log(subjectValue, emailValue, messageValue);
+    setSubmitMessage("Mail envoyé");
+  }
 
   return (
       <form id="contact" onSubmit={submit} className={`${styles.form}`}>
@@ -16,24 +25,25 @@ export default function ContactForm() {
             <legend className={`${styles.legend}`}>Vos informations</legend>
             <div className={`${styles.Field}`}>
               <label htmlFor="mean">Sujet du Contact</label>
-              <select name="mean" id="mean" className={`${styles.input}`}>
+              <select name="mean" id="mean" value={subjectValue} onChange={(e) => setSubjectValue(e.target.value)} className={`${styles.input}`}>
                   <option value="client">Demande de service</option>
                   <option value="other">autre</option>
               </select>
             </div>
             <div className={`${styles.Field}`}>
               <label htmlFor="email">Votre email</label>
-              <input type="text" name="email" id="email" className={`${styles.input}`}/>
+              <input type="text" name="email" id="email" value={emailValue} onChange={(e) => setEmailValue(e.target.value)} className={`${styles.input}`}/>
             </div>
         </fieldset>
         <fieldset className={`${styles.FieldSet}`}>
             <legend className={`${styles.legend}`}>Message</legend>
             <div className={`${styles.Field}`}>
               <label htmlFor="message">Contenu du message</label>
-              <textarea name="message" id="message" className={`${styles.input} ${styles.textarea}`}></textarea>
+              <textarea name="message" id="message" value={messageValue} onChange={(e) => setMessageValue(e.target.value)} className={`${styles.input} ${styles.textarea}`}></textarea>
             </div>
         </fieldset>
         <input type="submit" id="submit" className={`${styles.submit}`}/>
+        <p className={`${styles.submitMessage}`}>{submitMessage}</p>
       </form>
     )
 }
