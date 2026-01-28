7eedf9957ded0dc9a676e0fe0790579b9b4a8e7b
Lohos06 <122732120+Lohos06@users.noreply.github.com>
2026-01-28 12:55:57 +0100

    favIcon
    ajout de mon favIcon

--- app/Contact/page.tsx
+++ app/Contact/page.tsx
@@ -2,6 +2,13 @@ import Header from "@/components/Header/Header";
 
 import ContactForm from "@/components/ContactForm/ContactForm";
 
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Contact Lorenzo L'Hostis",
+  description: "Formulaire pour Contacter Lorenzo L'Hostis",
+};
+
 export default function Contact() {
   return (
     <div>
